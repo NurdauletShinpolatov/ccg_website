2b62a879672b00b91903a3db607be763f257421d
NurdauletShinpolatov <nshinpolatov@gmail.com>
2023-12-30 17:46:00 +0500

    added incomplete configurations

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,22 +1,31 @@
-import { observer } from 'mobx-react-lite';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import MainLayout from '../layouts/MainLayout';
-import { Home } from '../pages';
-import About from '../pages/about';
+import { observer } from "mobx-react-lite";
+import { Navigate, Route, Routes } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Main from "components/pages/Main";
+import Company from "components/pages/Company";
+import Services from "components/pages/Services";
+import ContactUs from "components/pages/ContactUs";
+import Careers from "components/pages/Careers";
+import Certifications from "components/pages/Certifications";
+import News from "components/pages/News";
 
 const Router = () => {
 
-	return (
-		<Routes>
-			<Route path="/" element={<MainLayout />}>
-				<Route index element={<Home />} />
-				<Route path="home" element={<Home />} />
-				<Route path="about" element={<About />} />
-			</Route>
-
-			<Route path="*" element={<Navigate to="/dashboard" />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Main />} />
+        <Route path="company" element={<Company />} />
+        <Route path="services" element={<Services />} />
+        <Route path="contact-us" element={<ContactUs />} />
+        <Route path="careers" element={<Careers />} />
+        <Route path="certifications" element={<Certifications />} />
+        <Route path="news" element={<News />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 };
 
 export default observer(Router);
